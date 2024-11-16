92bed80fa01a077a164f76b928c05cfc67c09192
Agness David <119672669+AgnessDavid@users.noreply.github.com>
2024-11-16 13:25:32 +0000

    CECI EST MON CODE

--- index5.php
+++ index5.php
@@ -1,41 +1,27 @@
 <?php
 session_start();
 
-$NombreD = 50;
+$nombreD = 50;
 $message = "";
 
- if(!isset($_SESSION['tentatives'])){
-
+// Initialiser le compteur de tentatives dans la session
+if (!isset($_SESSION['tentatives'])) {
   $_SESSION['tentatives'] = 0;
+}
 
- }
+if (isset($_POST['Envoyer'])) {
+  $Nombre = (int) $_POST['Nombre'];
+  $_SESSION['tentatives']++; // Incrémenter le nombre de tentatives stocké en session
 
-
-  if(isset($_POST['Envoyer'])){
-
-    $Nombre = (int) $_POST['Nombre'];
-    $_SESSION['tentatives']++;
-
-   if($Nombre < $NombreD ) {
-
-   $message = "C'est un nombre plus grand";
-
-   } elseif($Nombre > $NombreD) {
-
-    $message = "C'est un nombre plus petit";
-
-   } else {
-
-   $message = "BRAVO vous avez réussi en " .$_SESSION['tentatives']. "tentavites";
-   $_SESSION['tentatives'] = 0;
-
-   }
-
-
+  if ($Nombre < $nombreD) {
+    $message = "Ce nombre est plus grand";
+  } elseif ($Nombre > $nombreD) {
+    $message = "Ce nombre est plus petit";
+  } else {
+    $message = "FÉLICITATIONS ! VOUS AVEZ TROUVÉ LE BON NOMBRE en " . $_SESSION['tentatives'] . " tentatives";
+    $_SESSION['tentatives'] = 0; // Réinitialiser les tentatives après réussite
   }
-
-
-
+}
 
 ?>
 
@@ -43,29 +29,27 @@ $message = "";
 <html lang="fr">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Jeu du nombre mystère</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Jeu du nombre mystère</title>
 </head>
 
 <body>
 
-    <h1>Formulaire</h1>
+  <h1>Formulaire</h1>
 
-    <form action="" method="post">
-        <label for="Nombre">Entrer un nombre : </label>
-        <input type="number" name="Nombre" placeholder="Entrer un numéro" required>
-        <button type="submit" name="Envoyer">Envoyer</button>
-    </form>
+  <form action="" method="post">
+    <label for="Nombre">Entrer un nombre : </label>
+    <input type="number" name="Nombre" placeholder="Entrer un numéro" required>
+    <button type="submit" name="Envoyer">Envoyer</button>
+  </form>
 
-    <label for="Resultat">Résultat</label>
-    <input type="text" value="<?php echo htmlspecialchars($message); ?>" style="height: 100px; width: 900px;">
+  <label for="Resultat">Résultat</label>
+  <input type="text" value="<?php echo htmlspecialchars($message); ?>" style="height: 100px; width: 900px;">
 
-  <?php if($_SESSION['tentatives'] > 0)  : ?>
- 
+  <?php if ($_SESSION['tentatives'] > 0) : ?>
     <h3>Nombre de tentatives : <?php echo $_SESSION['tentatives']; ?> </h3>
-
-  <?php endif; ?> 
+  <?php endif; ?>
 
 </body>
 
