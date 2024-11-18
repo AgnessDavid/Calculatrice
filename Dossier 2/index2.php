9ced26b9ac5c88c147bc8fd85e1c354265e02f6d
Agness David <119672669+AgnessDavid@users.noreply.github.com>
2024-11-18 16:20:28 +0000

    CECI EST MON CODE

--- Dossier 2/index2.php
+++ Dossier 2/index2.php
@@ -6,9 +6,36 @@ for($i = 0; $i <= $Nombre; $i++){
 
 if($i % 2 == 0){
 
-    echo $i;
+    echo $i ."<br>";
 }
 
 }
+
+?>
+
+
+/* Les tableaux */
+
+<?php 
+
+$Fruits = ["dfh","fdshqsdf","hfqsfhgsfdh"];
+
+foreach($Fruits as $Fruit){
+echo $Fruit;
+}
+
+$LongF = "";
+
+foreach ($Fruits as $Fruit) {
+   
+if(strlen($Fruit) > strlen($LongF)){
+
+$LongF = $Fruit;
+
+}
+
+}
+
+echo $LongF;
 
 ?>
