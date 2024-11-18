63de9ed7c5ba1d5a50038d996c0862557c1656ca
Agness David <119672669+AgnessDavid@users.noreply.github.com>
2024-11-18 15:07:38 +0000

    CECI EST MON CODE

---
+++ Dossier 2/index.php
@@ -0,0 +1,31 @@
+<?php 
+
+$NombreA = "10";
+$NombreB = "12";
+
+
+$Addition = " $NombreA + $NombreB ";
+$Soustraction = " $NombreA - $NombreB ";
+$Multiplication = " $NombreA * $NombreB ";
+$Division = " $NombreA / $NombreB ";
+
+
+echo $Addition;
+echo $Soustraction;
+echo $Multiplication;
+echo $Division; 
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
